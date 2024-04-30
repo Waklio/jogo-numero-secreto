ebe892b7c506a4787c1e8b3bc0275eec402b7329
Waklio Neto <91980775+Waklio@users.noreply.github.com>
2024-04-29 23:40:46 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,167 @@
+function media(num1, num2) {
+    if (num1 > num2){
+        return num1
+    }else {
+
+    } return num2
+     
+
+}
+
+/*let digiteNumero = prompt('digite um numero inteiro');
+let contador = 0;
+while(contador <= digiteNumero){
+    console.log(contador);
+    contador++;
+    
+}
+*/
+/*
+let digiteNumero = prompt('digite um numero inteiro');
+let contador = 0;
+
+while(digiteNumero >= contador){
+    console.log(digiteNumero);
+    digiteNumero--;
+    
+}
+*/
+
+/*let contador = 10;
+
+while (contador <= 10){
+    console.log(contador);
+    contador--;
+  
+    break
+}
+*/
+/*let contador = 1;
+
+while (contador > 1 &&  contador <= 10){
+    console.log(contador);
+    contador++;
+  
+    
+}
+*/
+/*
+let aleatorio = parseInt(Math.random() * 1000);
+console.log(`${aleatorio}`);
+*/
+
+
+
+/*
+alert('Boas vindas ao jogo do número secreto');
+let numeroMaximo = 5000;
+let numeroSecreto = parseInt(Math.random() * numeroMaximo + 1);
+console.log(numeroSecreto);
+let chute; 
+let tentativas = 1;
+
+
+
+// enqaunto n for igual a numero secreto
+while (chute != numeroSecreto){
+    chute = prompt(`Escolha um número entre 1 e  ${numeroMaximo}`);
+    //chute toda a estrutura de if 
+    if (chute == numeroSecreto ){
+        break
+        
+    }else {
+        if (chute > numeroSecreto){
+            alert(`O número secreto é menor que ${chute}`);
+        }else{
+            alert(`O numero secreto é maior que ${chute}`);
+        }
+        tentativas++;
+    }
+}
+
+let palavraTentativa = tentativas > 1 ? ' tentativas' : 'tentativa';
+alert(`Isso aí! Você acertou o número secreto ${numeroSecreto} com ${palavraTentativa}.`);
+/*
+if( tentativas > 1 ){
+    alert(`Isso aí! Você acertou o número secreto ${numeroSecreto} com ${tentativas} tentativas. `);
+}else {
+    alert(`Isso aí! Você acertou o número secreto ${numeroSecreto} com ${tentativas} tentativa.`);
+    
+}
+
+
+*/
+
+
+
+
+/*let nome = prompt('Digite seu Nome');
+
+alert(`Seja muito bem vindo ${nome} `);
+
+*/
+
+/*let saldo = 30000
+
+alert(`o seu saldo é de ${saldo}`);
+*/
+
+
+/*let numero = prompt('Digite um numero inteiro');
+
+if (numero >= 100){
+    alert('Parabéns, você venceu!');
+}else{ (numero < 100)
+    alert('Tente novamente para ganhar');
+}
+
+*/
+
+
+/* 2let numero = prompt('Digite um numero inteiro podendo ser positivo ou negativo');
+
+if (numero >= 0){
+    alert('Numero Positivo');
+}else if (numero <= 0){
+    alert('Número Negativo');
+}else{
+    alert('Nem positivo e nem Negativo');
+}
+*/
+
+/*1 let diaDaSemana = prompt('Qual o dia da semana?');
+
+
+if (diaDaSemana == 'Sábado'){
+    alert('Bom Fim de Semana')
+}else if (diaDaSemana =='Domingo'){
+    alert('Bom Fim de Semana');
+}else {
+    alert('Boa semana')
+}
+*/
+
+
+
+
+
+
+/*let nome = lua;
+let idade = 25;
+let numeroDeVendas = 50;
+let saldoDisponivel = 1000;
+let mensagemDeErro = 'Erro!'
+
+alert('Erro!');
+
+let nome = prompt ('Qual é o seu nome?');
+let idade = prompt ('Qual é a sua idade?');
+
+if( idade >= 18){
+    alert('Pode tirar a habilitação');
+    else {
+        alert('Menor n pode');
+    }
+  
+}
+*/
